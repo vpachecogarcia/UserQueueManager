43d4f358ea4cb0cd631fe27daf23221ce90bbdc1
Irene <irene.nus8@gmail.com>
2018-12-18 12:44:54 +0100

    Waitingtime: Miramos que usuario tiene el mismo nombre en firebase que mi nombre, obtenemos su id y le añadimos el waiting time de momento se añade un valor constante hay que obtener el slot time y el numuser de su cola y multiplicarlos

--- app/src/main/java/victor/pacheco/userqueuemanager/UserQueueActivity.java
+++ app/src/main/java/victor/pacheco/userqueuemanager/UserQueueActivity.java
@@ -1,10 +1,12 @@
 package victor.pacheco.userqueuemanager;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
@@ -15,6 +17,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firestore.v1beta1.StructuredQuery;
 
@@ -44,6 +47,28 @@ public class UserQueueActivity extends AppCompatActivity {
         waiting_time = findViewById(R.id.waiting_time);
         queueId = getIntent().getStringExtra("queueId");
         username = getIntent().getStringExtra("username");
+
+        // Miramos que usuario tiene el mismo nombre en firebase que mi nombre, obtenemos su id y le añadimos el waiting time
+        // de momento se añade un valor constante hay que obtener el slot time y el numuser de su cola y multiplicarlos
+        // Comento como obtendria yo el Slot time pero creo que esta mal
+
+        db.collection("Queues").document(queueId).collection("Users")
+                .whereEqualTo("usr_id", username)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                // Queue q = doc.toObject(Queue.class);
+                                // int s = q.getSlot_time();
+
+                                db.collection("Queues").document(queueId).collection("Users").document(doc.getId()).update("waiting_time",2);
+                            }
+                        }
+                    }
+                });
+
 
 
 
